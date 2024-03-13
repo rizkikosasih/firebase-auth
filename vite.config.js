e119f903ae03e4cc10b3b729902084adb10d54d2
Rizki Kosasih <rizkigames9@gmail.com>
2024-03-13 23:21:35 +0700

    remove vercel plugin and add vercel json

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import vercel from 'vite-plugin-vercel';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/firebase-auth/',
-  plugins: [react(), vercel()],
+  plugins: [react()],
   server: {
     host: true
   }
