f565b27944d7c0581f7b9e8eed0afee508ae9c54
Rizki Kosasih <rizkigames9@gmail.com>
2024-03-13 23:43:54 +0700

    update hashrouter to browserrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Home, Login, Register, PageNotFound } from './pages';
 
 const App = () => {
